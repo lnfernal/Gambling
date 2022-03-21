30c04ebb1def7e4a9ef55d0f01b5c73205f7a254
Pevo <38362954+Przemek19@users.noreply.github.com>
2022-03-21 22:11:42 +0100

    .

---
+++ Website/client/src/components/BetPanel.vue
@@ -0,0 +1,64 @@
+<template>
+  <div :class="$style.betPanelContainer">
+    <input type="number" v-model="value" aria-label="Bet value" />
+    <div :class="$style.inputAddon"></div><div :class="$style.buttonAddon"></div>
+    <button>Bet</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: 0,
+    };
+  },
+};
+</script>
+
+<style module lang="scss">
+  .betPanelContainer {
+    width: fit-content;
+    height: fit-content;
+    padding: 8px;
+    box-sizing: border-box;
+    background-color: var(--dark3NA);
+    display: flex;
+    position: relative;
+    .inputAddon, .buttonAddon {
+      position: absolute;
+      height: 37px;
+      transform: skewX(-20deg);
+    }
+    .inputAddon {
+      left: 128px + 16px;
+      width: 16px;
+      background-color: var(--dark5NA);
+    }
+    .buttonAddon {
+      left: 128px + 16px + 16px;
+      width: 16px;
+      background-color: var(--defaultColor);
+    }
+    button {
+      background-color: var(--defaultColor);
+      outline: 0;
+      border: 0;
+      font-size: 16px;
+      width: 64px + 16px;
+      z-index: 1;
+      padding-right: 12px;
+    }
+    input {
+      z-index: 1;
+      outline: 0;
+      border: 0;
+      box-sizing: border-box;
+      padding: 8px;
+      font-size: 16px;
+      width: 128px + 16px;
+      background-color: var(--dark5NA);
+      margin-right: 16px;
+    }
+  }
+</style>
--- Website/client/src/views/pages/wheel/main.vue
+++ Website/client/src/views/pages/wheel/main.vue
@@ -1,3 +1,73 @@
 <template>
-  <div></div>
+  <div>
+    <div :class="$style.topContainer">
+      <div :class="$style.betPanelContainer">
+        <BetPanel />
+      </div>
+      <div :class="$style.wheelContainer">
+        <img src="@/assets/wheel.png" alt="wheel" :style="`transform: rotate(${rotation}deg)`" />
+        <img src="@/assets/wheel.png" alt="wheel" :style="`transform: rotate(${rotation}deg)`" />
+        <img src="@/assets/wheel.png" alt="wheel" :style="`transform: rotate(${rotation}deg)`" />
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+import BetPanel from '../../../components/BetPanel.vue';
+
+export default {
+  components: {
+    BetPanel,
+  },
+  data() {
+    return {
+      rotation: 0,
+    };
+  },
+};
+</script>
+
+<style module lang="scss">
+  .topContainer {
+    width: 100%;
+    display: inline-flex;
+    flex-direction: row-reverse;
+
+    .betPanelContainer {
+      width: 50%;
+      height: auto;
+      flex-basis: 50%;
+      display: inline-flex;
+      justify-content: center;
+      align-items: center;
+    }
+    .wheelContainer {
+      width: 50%;
+      flex-basis: 50%;
+      padding: 32px;
+      box-sizing: border-box;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      img:first-child, img:nth-child(2) {
+        position: absolute;
+        filter: blur(12px);
+      }
+      img {
+        width: 256px + 128px;
+      }
+    }
+  }
+
+  @media only screen and (max-width: 1100px) {
+    .topContainer {
+      display: flex;
+      flex-direction: column-reverse;
+      .betPanelContainer, .wheelContainer {
+        width: 100%;
+        flex-basis: 100%;
+      }
+    }
+  }
+</style>
